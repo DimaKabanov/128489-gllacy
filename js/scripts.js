b640608ab911f6cdc5421015f90d0464afa07929
Dmitry Kabanov <dkabanov6@gmail.com>
2016-04-27 07:53:45 +0500

    Сделал смену фона сайта и подправил стили

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,44 @@ var btn = contacts.querySelector(".btn");
 var feedbackForm = document.querySelector(".feedback-form");
 var closeForm = document.querySelector(".feedback-form-close");
 var name = feedbackForm.querySelector("[name=name]");
+var overlay = document.querySelector(".overlay");
+var slideFirst = document.querySelector("#slideFirst");
+var slideSecond = document.querySelector("#slideSecond");
+var slideThree = document.querySelector("#slideThree");
 
 btn.addEventListener("click", function(event){
   event.preventDefault();
   feedbackForm.classList.add("feedback-form-show");
+  overlay.classList.add("overlay-show");
   name.focus();
 });
 
 closeForm.addEventListener("click", function(event){
   event.preventDefault();
   feedbackForm.classList.remove("feedback-form-show");
+  overlay.classList.remove("overlay-show");
 });
+
+window.addEventListener("keydown", function(event){
+  if (event.keyCode === 27) {
+    if (feedbackForm.classList.contains("feedback-form-show")) {
+      feedbackForm.classList.remove("feedback-form-show");
+      overlay.classList.remove("overlay-show");
+    }
+  }
+});
+
+slideFirst.onclick = function() {
+  document.body.style.backgroundColor="#849d8f";
+};
+
+slideSecond.onclick = function() {
+  document.body.style.backgroundColor="#8996a6";
+};
+
+slideThree.onclick = function() {
+  document.body.style.backgroundColor="#9d8b84";
+};
 
 //map
 function initialize() {
