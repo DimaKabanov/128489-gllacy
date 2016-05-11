bef6216be6b547f2c2c2805c1d75adc9c43ccf22
Dmitry Kabanov <dkabanov6@gmail.com>
2016-05-11 08:01:14 +0500

    Поправил слайдер и ссылки

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,8 @@ window.addEventListener("keydown", function(event){
   }
 });
 
+//slider
+
 slideFirst.onclick = function() {
   document.body.style.backgroundColor="#849d8f";
 };
@@ -43,12 +45,13 @@ slideThree.onclick = function() {
 };
 
 //map
+
 function initialize() {
   var mapOptions = {
     scrollwheel: false,
     zoom: 17,
     center: new google.maps.LatLng(59.9389678,30.327000)
-  }
+  };
   var map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
   var image = 'img/marker.png';
